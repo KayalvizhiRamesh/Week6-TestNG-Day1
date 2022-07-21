796de6e00ef9a2b82f7a07fedc8e6467bfcf3f67
KayalvizhiRamesh <53007703+KayalvizhiRamesh@users.noreply.github.com>
2022-07-21 18:10:25 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,26 @@
+package testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass {
+
+	@Test(priority = 1, invocationCount = 3, threadPoolSize = 3, invocationTimeOut = 1000)
+
+	public void runCreateLead() throws InterruptedException {
+
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+}
